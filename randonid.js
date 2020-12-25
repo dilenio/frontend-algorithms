2808ffe51f9ff6ca67d5cd03928be6689ba96f49
Dilenio Enderle <dilenio@gmail.com>
2020-12-25 08:01:49 -0300

    final commit

--- randonid.js
+++ randonid.js
@@ -2,6 +2,7 @@ const generateId = () => {
   const alphanumeric = '0123456789abcdefghijklmnopqrstuvwxyz';
 
   let generated = '';
+
   for (let index1 = 0; index1 < 4; index1++) {
     for (let index2 = 0; index2 < 4; index2++) {
       generated += alphanumeric.charAt(
