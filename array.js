c7933c35efc09fc8c1f41f173e63dc9fc2682c20
Dilenio Enderle <dilenio@gmail.com>
2020-12-25 07:21:16 -0300

    Create a hashmap that contains the elementary functions of addition, subtraction, multiplication and division

--- array.js
+++ array.js
@@ -1,8 +1,3 @@
-// Crie um algoritmo que percorre uma array unidimensional contendo letras e números: [ “a”, 10, “b”, “hola”, 122, 15]
-// Obtenha uma array contendo apenas as letras
-// Obtenha uma array contendo apenas os números
-// Obtenha o maior número da array acima
-
 const uniArray = ['a', 10, 'b', 'hola', 122, 15];
 
 const contentArray = (array, type) => {
---
+++ hashmap.js
@@ -0,0 +1,15 @@
+const hashMap = {
+  addition: (array) => array.reduce((acc, curr) => acc + curr),
+  subtraction: (array) => array.reduce((acc, curr) => acc - curr, 0),
+  multiplication: (array) => array.reduce((acc, curr) => acc * curr),
+  division: (a, b) => {
+    if (b === 0) return 'Can not divide by zero';
+    return a / b;
+  },
+};
+
+console.log(hashMap.addition([1, 2, 3, 4]));
+console.log(hashMap.subtraction([1, 2, 3, 4]));
+console.log(hashMap.multiplication([1, 2, 3, 4]));
+console.log(hashMap.division(22, 0));
+console.log(hashMap.division(543, 46));
